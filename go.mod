fe15e944c5d0a072d303725b523b9530533b715d
kalawytest <kalawytest1@gmail.com>
2025-04-11 23:05:13 +0200

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kalawytest/xx
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,9 @@
+//go:build !installhook
+// +build !installhook
+
+package awesomeutils
+
+// Normal package code
+func Helper() string {
+    return "I'm a helpful function!"
+}
